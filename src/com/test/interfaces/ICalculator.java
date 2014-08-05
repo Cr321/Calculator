19509608de0820230c102969922e2fb58c27824e
Cairui <Cairui@Cairui-PC>
2014-08-05 13:42:32 +0800

    添加注释

--- src/com/test/interfaces/ICalculator.java
+++ src/com/test/interfaces/ICalculator.java
@@ -2,8 +2,10 @@ package com.test.interfaces;
 
 public interface ICalculator {
 
+	//将数字压栈
 	public void pushOperand(String operand);
 	
+	//将操作符压榨
 	public double pushOperate(String operate);
 	
 	public void reset();
